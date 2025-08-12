b80dc522d68c7e0c4c3d8e8109997872ab9b2740
LucasESGomes <lucasgomesferreira2008@gmail.com>
2025-08-12 15:33:18 -0300

    otavioIssue

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { createBrowserRouter, RouterProvider} from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
+
+//Pages 
 import Login from './pages/login.jsx';
 
 import './index.css';
 
+const router = createBrowserRouter([
+  {path: '/', element: <h2>Hello World</h2>},
+  {path: '/login', element: <Login />}
+]);
+
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Login />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
