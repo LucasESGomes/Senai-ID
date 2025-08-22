907d22711ce4e3a17df46efa5a368492f94ad22f
LucasESGomes <lucasgomesferreira2008@gmail.com>
2025-08-22 16:57:20 -0300

    delayForm

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 
+import DelayManagement from "../components/containers/delayManagement.jsx";
 import LoggedHeader from "../components/layout/loggedHeader.jsx";
 import Footer from "../components/layout/footer.jsx"
 
@@ -9,6 +10,7 @@ function Register() {
         <>
             <LoggedHeader />
 
+            <DelayManagement />
         </>
     )
 }
