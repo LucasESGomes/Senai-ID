0d49cb8bf4c519cd8047f22b6481453b2fc8140a
lucasesgomes <lucasgomesferreira2008@gmail.com>
2025-09-23 15:57:43 -0300

    userAuth + login integration completed - missing routes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-	  baseURL: 'https://senai-id-1.onrender.com/api/v1/users',
+	  baseURL: 'https://senai-id-1.onrender.com/api/v1',
 });
 
 export const setAuthToken = (token) => {
